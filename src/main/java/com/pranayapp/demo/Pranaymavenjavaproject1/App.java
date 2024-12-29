dd8f313aa6dbf9b9b2dc204baf7fd3b11c0935ed
Pranay090 <chpranay1109.kumar@gmail.com>
2024-12-29 22:44:44 +0530

    checking everything is done?

---
+++ src/main/java/com/pranayapp/demo/Pranaymavenjavaproject1/App.java
@@ -0,0 +1,13 @@
+package com.pranayapp.demo.Pranaymavenjavaproject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
